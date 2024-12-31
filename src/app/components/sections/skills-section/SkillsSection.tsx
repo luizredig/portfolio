684e67eceb6ca1afd8a6818ebf595e19cfaf6d0c
redig <luizredig@gmail.com>
2024-12-31 02:43:43 -0300

    feat(components): add SkillsSection

---
+++ src/app/components/sections/skills-section/SkillsSection.tsx
@@ -0,0 +1,52 @@
+import { prismaClient } from "@/app/lib/prisma";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../../ui/carousel";
+import Section from "../Section";
+import SkillCard from "./SkillCard";
+
+const SkillsSection = async () => {
+  const skills = await prismaClient.skill.findMany({
+    orderBy: { name: "asc" },
+  });
+
+  return (
+    <>
+      <Section>
+        <div className="bg-dot absolute inset-0 flex h-full flex-1 opacity-20">
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]"></div>
+        </div>
+
+        <div className="flex w-full flex-col items-center justify-center px-16 md:px-36">
+          <p className="text-3xl font-bold">Redig's skills</p>
+
+          <div className="flex w-full justify-center">
+            <Carousel orientation="horizontal" className="w-full">
+              <CarouselContent className="w-full">
+                {skills &&
+                  skills.map((skill) => (
+                    <CarouselItem
+                      key={skill.id}
+                      className="basis-full lg:basis-1/4"
+                    >
+                      <SkillCard skill={skill} />
+                    </CarouselItem>
+                  ))}
+              </CarouselContent>
+
+              <CarouselPrevious />
+
+              <CarouselNext />
+            </Carousel>
+          </div>
+        </div>
+      </Section>
+    </>
+  );
+};
+
+export default SkillsSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import "@/app/css/scrollbar.css";
+import "@/app/css/backgrounds/grid.css";
 
 import IntroSection from "./components/sections/intro-section/IntroSection";
 import ProjectsSection from "./components/sections/projects-section/ProjectsSection";
+import SkillsSection from "./components/sections/skills-section/SkillsSection";
 
 const Page = () => {
   return (
     <>
-      <div className="flex h-full snap-y snap-mandatory flex-col overflow-y-auto [&::-webkit-scrollbar]:hidden">
+      <div className="flex h-full snap-y snap-mandatory flex-col overflow-y-auto">
         <IntroSection />
+
+        <SkillsSection />
 
         <ProjectsSection />
       </div>
